3f554a67940cc2f70e1ecc2e08b15f727519783c
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2026-04-27 20:20:59 +0530

    fixed

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,14 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://ankitnathtiwari6.github.io",
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+  credentials: true,
+}));
 app.use(helmet());
 
 // Logging in development environment
